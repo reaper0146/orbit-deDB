53590d07ef07400a64b5be58e012d003b5a658d3
soumyapal96 <pal.soumya1996@gmail.com>
2021-10-01 14:33:47 -0400

    update

--- server/server.js
+++ server/server.js
@@ -14,51 +14,59 @@ const initIPFSInstance = async () => {
     console.log('hoi')
     return await IPFS.create({ repo: "./path-for-js-ipfs-repo" });
   };
-  
-  initIPFSInstance().then(async ipfs => {
 
+  async function init(){
     const ipfsOptions = {
         EXPERIMENTAL: {
           pubsub: true
         }
       }
-
-    const ipfs = await IPFS.create(ipfsOptions)  
-    //const orbitdb = await OrbitDB.createInstance(ipfs);
-  
-    // Create / Open a database
-    //const db = await orbitdb.keyvalue('test-db')
-    //await db.load();
-
-    //const db = orbitDB.keyvalue('test-db')
-    //console.log(db.address.toString())
-
-    const identity = await Identities.createIdentity(options)
-    console.log(identity.toJSON())
-    const orbitdb = await OrbitDB.createInstance(ipfs,  { identity: identity })
- 
-    const db = orbitDB.keyvalue('test-db')
-    await db.put({ _id: 'test', name: 'test-doc-db', category: 'distributed' })
-
-    const address = db.address.toString()
-    console.log(address)
-    await db2.load()
-    const value = db2.get()
-    console.log(value)
-    // Listen for updates from peers
-    //db.events.on("replicated", address => {
-    //  console.log(db.iterator({ limit: -1 }).collect());
-    //});
-  
-    // Add an entry
-    //const hash = await db.add("world");
-    //console.log(hash);
-  
-    // Query
-    //const result = db.iterator({ limit: -1 }).collect();
-    //console.log(JSON.stringify(result, null, 2));
-  });
-  
+    
+        const ipfs = await IPFS.create(ipfsOptions)  
+        //const orbitdb = await OrbitDB.createInstance(ipfs);
+      
+        // Create / Open a database
+        //const db = await orbitdb.keyvalue('test-db')
+        //await db.load();
+    
+        //const db = orbitDB.keyvalue('test-db')
+        //console.log(db.address.toString())
+    
+        //const identity = await Identities.createIdentity(options)
+        //console.log(identity.toJSON())
+        const orbitDB = await OrbitDB.createInstance(ipfs)//,  { identity: identity })
+     
+        const db = await orbitDB.docs('test-db')
+        console.log(db.address.toString())
+        //await db.put({ _id: 'test', name: 'test-doc-db', category: 'distributed' })
+        await db.put({'_id': 'QmBwesomeIpfsHash', name:'Jack', age: 20})
+        await db.put({'_id': 'QmCwesomeIpfsHash', name:'Aaron', age: 21})
+        await db.put({'_id': 'QmDwesomeIpfsHash', name:'John', age: 22})
+        await db.put({'_id': 'QmEwesomeIpfsHash', name:'Bob', age: 23})
+        await db.put({'_id': 'QmFwesomeIpfsHash', name:'Alice', age: 29})
+        console.log("hello")
+    
+        const address = db.address.toString()
+        console.log(address)
+        await db.load()
+        const value = db.query((doc) => doc.age >= 20)
+        
+        console.log(value)
+        // Listen for updates from peers
+        //db.events.on("replicated", address => {
+        //  console.log(db.iterator({ limit: -1 }).collect());
+        //});
+      
+        // Add an entry
+        //const hash = await db.add("world");
+        //console.log(hash);
+      
+        // Query
+        //const result = db.iterator({ limit: -1 }).collect();
+        //console.log(JSON.stringify(result, null, 2));
+      //});
+    
+  }
 
 const db = mysql.createConnection({
     user:"root",
@@ -72,7 +80,7 @@ app.post('/register', (req,res)=> {
     const password = req.body.password
     console.log(username)
     console.log(password)
-    initIPFSInstance()
+    init()
 
     /*db.query("INSERT INTO users (username, password) VALUES (?,?)", [username, password], 
     (err,result) => {console.log(err);}
